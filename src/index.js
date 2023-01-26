e48cca0962c0109aa00861a59ce4a6e6c07c131a
alex1779 <alex1779@gmail.com>
2023-01-25 21:04:03 -0300

    Modification required by review.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
 import './style.css';
 
-const tasksArray = [];
-
-function createTask(obj) {
-  let myindex;
-  if (tasksArray.length === 0) {
-    myindex = 0;
-  } else {
-    myindex = tasksArray.length;
-  }
-
-  const task = {
-    description: obj,
+const tasks = [
+  {
+    index: 0,
     completed: false,
-    index: myindex,
-  };
-  tasksArray.push(task);
+    description: 'review this project',
+  },
+  {
+    index: 1,
+    completed: true,
+    description: 'aprobe this project',
+  },
+];
 
-  const contenedor = document.querySelector('#contenedorTasks');
-  contenedor.innerHTML += `<li>
-  <div class="taskCont">
-    <input type="checkbox">
-    <div class="task">${task.description}</div>
-  </div>
-  <div class="icon">&#8942</div>
-</li>
+const fillList = () => {
+  const container = document.querySelector('.todo-list');
+  tasks.forEach((todo) => {
+    container.innerHTML += `<li>
+    <div class="taskCont">
+      <input type="checkbox">
+      <div class="task">${todo.description}</div>
+    </div>
+    <div class="icon">&#8942</div>
+  </li>
 `;
-  const inputText = document.querySelector('#inputNewTask');
-  inputText.value = '';
-}
-
-window.onload = () => {
-  // const button = document.querySelector('.clearAll');
-  // button.onclick = function name() { sayHi(); };
-
-  const inputText = document.querySelector('#inputNewTask');
-  inputText.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13) { createTask(inputText.value); }
   });
 };
+fillList();
